f5b4736f7a2d2b6f1c1e4481fb30c4c73ce525b0
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-01-20 18:30:29 +0900

    [DROOLS-4561] DMN introspect PMML for output types

--- kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/pmml/DMNKiePMMLInvocationEvaluator.java
+++ kie-dmn/kie-dmn-core/src/main/java/org/kie/dmn/core/pmml/DMNKiePMMLInvocationEvaluator.java
@@ -17,6 +17,7 @@
 package org.kie.dmn.core.pmml;
 
 import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -28,11 +29,13 @@ import org.kie.api.pmml.PMML4Field;
 import org.kie.api.pmml.PMML4Result;
 import org.kie.dmn.api.core.DMNMessage;
 import org.kie.dmn.api.core.DMNResult;
+import org.kie.dmn.api.core.DMNType;
 import org.kie.dmn.api.core.event.DMNRuntimeEventManager;
 import org.kie.dmn.core.api.EvaluatorResult;
 import org.kie.dmn.core.api.EvaluatorResult.ResultType;
 import org.kie.dmn.core.ast.DMNFunctionDefinitionEvaluator.FormalParameter;
 import org.kie.dmn.core.ast.EvaluatorResultImpl;
+import org.kie.dmn.core.impl.CompositeTypeImpl;
 import org.kie.dmn.core.impl.DMNResultImpl;
 import org.kie.dmn.core.util.Msg;
 import org.kie.dmn.core.util.MsgUtil;
@@ -76,6 +79,48 @@ public class DMNKiePMMLInvocationEvaluator extends AbstractPMMLInvocationEvaluat
         PMML4Result resultHolder = helper.submitRequest(request.build());
 
         Map<String, Object> resultVariables = resultHolder.getResultVariables();
+
+        Optional<DMNType> opt = getCompositeOutput();
+        if (opt.isPresent()) {
+            CompositeTypeImpl type = (CompositeTypeImpl)opt.get();
+            Map<String, Object> result = new HashMap<>();
+            for (Entry<String, Object> kv : resultVariables.entrySet()) {
+                Object r = kv.getValue();
+                if (r instanceof PMML4Field) {
+                    final String resultName = kv.getKey();
+                    if (resultName != null && !resultName.isEmpty()) {
+                        Optional<String> outputFieldNameFromInfo = type.getFields()
+                                                                       .keySet()
+                                                                       .stream()
+                                                                       .filter(k -> k.equalsIgnoreCase(resultName))
+                                                                       .findFirst();
+                        if (outputFieldNameFromInfo.isPresent()) {
+                            String name = outputFieldNameFromInfo.get();
+                            try {
+                                Method method = r.getClass().getMethod("getValue");
+                                Object value = method.invoke(r);
+                                result.put(name, EvalHelper.coerceNumber(value));
+                            } catch (Throwable e) {
+                                MsgUtil.reportMessage(LOG,
+                                                      DMNMessage.Severity.WARN,
+                                                      node,
+                                                      ((DMNResultImpl) result),
+                                                      e,
+                                                      null,
+                                                      Msg.INVALID_NAME,
+                                                      name,
+                                                      e.getMessage());
+                                result.put(name, null);
+                            }
+                        }
+                    }
+                }
+            }
+
+            Object coercedResult = result.size() > 1 ? result : result.values().iterator().next();
+            return new EvaluatorResultImpl(coercedResult, ResultType.SUCCESS);
+        }
+
         Map<String, Object> result = new HashMap<>();
         for (Entry<String, Object> kv : resultVariables.entrySet()) {
             Object r = kv.getValue();
@@ -114,6 +159,20 @@ public class DMNKiePMMLInvocationEvaluator extends AbstractPMMLInvocationEvaluat
 
         Object coercedResult = result.size() > 1 ? result : result.values().iterator().next();
         return new EvaluatorResultImpl(coercedResult, ResultType.SUCCESS);
+    }
+
+    private Optional<DMNType> getCompositeOutput() {
+        Collection<? extends PMMLModelInfo> models = pmmlInfo.getModels();
+        Optional<DMNType> opt = models.stream()
+                .filter(m -> model.equals(m.getName()))
+                .filter(m -> m instanceof DMNPMMLModelInfo)
+                .flatMap(m -> ((DMNPMMLModelInfo)m).getOutputFields().entrySet().stream())
+                .filter(e -> e.getKey().equals(model))
+                .filter(e -> e.getValue() instanceof CompositeTypeImpl)
+                .map(e -> e.getValue())
+                .findFirst();
+        return opt;
+
     }
 
 }
